93de3fa867d2a53cbec4704a7b1deed875b8d89a
Edwin Hoogerbeets <edwin.hoogerbeets@healthtap.com>
2017-11-09 13:37:35 -0800

    Initial check-in. The loctool is split from the ilib-js/ilib project.

---
+++ test/testfiles/getstring.js
@@ -0,0 +1,7 @@
+rb.getString("This string should be extracted.");
+rb.getStr("This string should not be extracted.");
+getString("This string should not be extracted either.");
+rb.GetString("This function call has the wrong case.");
+rb.getString ( "This string should be extracted." );
+
+rb.getString("This is a new string.");
